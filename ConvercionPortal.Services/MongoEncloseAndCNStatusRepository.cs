30e0c06d5f34385cf3127b15a297d8466c27e768
azirumga <dubledorm@gmail.com>
2022-03-13 11:28:38 +0300

    Добавлен пакет MongoDB в проект ConversionPortal.Services. Сделана заготовка для класса MongoEncloseAndCNStatusRepository

---
+++ ConvercionPortal.Services/MongoEncloseAndCNStatusRepository.cs
@@ -0,0 +1,50 @@
+﻿using ConvercionPortal.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvercionPortal.Services
+{
+    internal class MongoEncloseAndCNStatusRepository : ScopedRepository<EncloseAndCNStatus>, IEncloseAndCNStatusRepository
+    {
+        private readonly MongoClient _client;
+        IMongoDatabase _database;
+
+
+        public MongoEncloseAndCNStatusRepository(string mongoConnectionString, string mongoDatabaseName) : base()
+        {
+            _client = new MongoClient(mongoConnectionString);
+            _database = _client.GetDatabase(mongoDatabaseName);
+            //  AddScope("Name", ScopeByName);
+            //  AddScope("Description", ScopeByDescription);
+        }
+
+        public EncloseAndCNStatus? Delete(int id, int ownerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<EncloseAndCNStatus> GetAll(Dictionary<string, string> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EncloseAndCNStatus? GetById(int id, int ownerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EncloseAndCNStatus? Insert(EncloseAndCNStatus encloseAndCNStatus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EncloseAndCNStatus? Update(EncloseAndCNStatus encloseAndCNStatus)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
